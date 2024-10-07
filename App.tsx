0a9bcdc6b2a896e23eb97b7dd5c64b3ca7fd7fa4
priyam <priyam>
2024-10-07 19:13:35 +0530

    route add

--- App.tsx
+++ App.tsx
@@ -28,6 +28,9 @@ import {
 import getColors from './src/Themes/Color';
 import Register from './src/Pages/Register';
 import Login from './src/Pages/Login';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import StackNavigation from './src/Navigation/StackNavigation';
 const colors = getColors('custom');
 
 type SectionProps = PropsWithChildren<{
@@ -66,40 +69,16 @@ function App(): React.JSX.Element {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
-
+  const Stack = createNativeStackNavigator();
   return (
     <SafeAreaView style={{backgroundColor: colors.background, flex: 1}}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <Register />
-      {/* <Login /> */}
-
-      {/* <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView> */}
+      <NavigationContainer>
+        <StackNavigation />
+      </NavigationContainer>
     </SafeAreaView>
   );
 }
